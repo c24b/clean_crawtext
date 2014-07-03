e7ce31df7c9100b12686bfa44b8d451a3dd0a270
C2_4b <4barbes@gmail.com>
2014-07-03 15:07:18 +0200

    adding job scheduling with refined options

--- scheduler3.py
+++ scheduler3.py
@@ -5,7 +5,7 @@ from database import Database, TASK_MANAGER_NAME, TASK_COLL
 import re
 from datetime import datetime
 from abc import ABCMeta, abstractmethod
-from page2 import Page
+#from page2 import Page
 from validate_email import validate_email
 import docopt
 from utils import yes_no
@@ -21,72 +21,50 @@ class Scheduler(object):
 		'''Schedule a new job from user_input (crawtext.py)'''
 		j = Job.create_from_ui(user_input)
 		# selecting user
-		if j['user'] is not None:
-			#show all the project of the owner
-			if self.select_job(j, "user") is None:
-				print "User:%s is not already registered" %j["user"]
-				print "To register you as user:\n1/ Create a new project: python crawtext.py mynewproject\n2/Set Ownership: python crawtext.py mynewproject -u you@cortext.net" %j["user"]
+		if j['user'] is not None and j['name'] is None:
+			#find user
+			#if user
+			if self.get_one({"user":j["user"]}) is not None:
+				#show every projects of the user
+				print "Project owned by %s"%j["user"] 
+				self.show_by(j,"user")
+				return True
 			else:
-				self.select_job(j,"user")
-				return 
-			#enter with single project name					
-			if j['name'] is not None and j['action'] is None: 	
+				#error_msg
+				print "User:%s is not already registered" %j["user"]
+				print "To register you as user %s:\n1/Create a new project:\n\tpython crawtext.py yournewproject\n2/Set Ownership to the project:\n\tpython crawtext.py yournewproject -u %s" %(j["user"],j["user"])
+				return False
+		#selecting project
+		elif j["name"] is not None and j['update'] is False:
+				#verifying that name is correct
 				if j['name'] in ["crawl", "delete", "archive", "report", "export"]:
 					print "**Project Name** can't be 'crawl', 'archive', 'report', 'export' or 'delete'"
 					print "\t*To generate a report:\n\t\tcrawtext report pesticides"
 					print "\t*To create an export :\n\t\tcrawtext export pesticides"
 					print "\t*To delete a projet :\n\t\tcrawtext delete pesticides"
-					return
-				elif j['action'] is not None:
-					if j['action'] == "delete":
-						return self.delete({"name":j["name"]})
-					
-					elif j['action'] == "update":
-						print j['update']
-						existing_job = self.get_one({"name":j["name"],"action":"crawl"})				
-						if existing_job is not None:
-							for k,v in j.items():
-								if v is not False and v is not None:
-									existing_job[k] = v
-							existing_job["date"].append(datetime.now())
-							self.collection.update({"_id":existing_job["_id"]},existing_job, upsert=True) 
-							print "Project %s successfully updated with new %s options !"%(j['name'], j['action'])
-							return True
-					else:
-						self.collection.insert(j)
-						print "Job %s has been successfully scheduled in project %s!"%(j['action'],j['name'])
-						return True
-				elif j['update'] is not None:
-					print "Update"
-					print j['update']
-					existing_job = self.get_one({"name":j["name"]})				
-					if existing_job is not None:
-						for k,v in j.items():
-							if v is not False and v is not None:
-								existing_job[k] = v
-						existing_job["date"].append(datetime.now())
-						existing_job["action"] = "crawl"
-						self.collection.update({"_id":existing_job["_id"]},existing_job, upsert=True) 
-						print "Project %s successfully updated with new %s options !"%(j['name'], j['action'])
+					print "\t*To archive a website :\n\t\tcrawtext archive www.lemonde.fr"
+					return False
+				#project doesn't exist: create new one
+				elif self.get_one({"name":j['name']}) is None:							
+					print "No existing project found!"
+					j['action'] = "create"
+					j2 = Job.create_from_database(j)
+					j2.run()
+					return True
+				#project exist: show
 				else:
-					if self.get_one({"name":j['name']}) is None:							
-						print "No existing project found! "
-						new = yes_no("Do you want to create a new CRAWL project with your parameters ?")
-						if new == 1:
-							j['action'] = "crawl"
-							self.collection.insert(j)
-							print "Project %s has been successfully created and set on %s! " %(j["name"], j["action"])
-							return True
-					#project exists
-					else:
-						if j['email'] is not None:
-							print "Project %s will be owned by %s" %(j['name'], j['email'])
-							for task in self.get_list({"name":j["name"]}):
-								self.collection.update(task,{"$set":{"email": j["email"]}})
-							return self.select_job(j, "email")
-						else:
-							return self.select_job({"name":j['name']})
-				
+					print "Jobs of the project %s"%j["name"] 
+					self.show_by(j, "name")
+					return True
+		
+		else:
+			if j["update"] is True:
+				print "Update existing project"
+				j2 = Job.create_from_database(j)
+				j2.run()
+			#j['action'] is not None:
+			#create job
+			
 		
 	def delete(self, job_name):
 		'''Delete existing project'''
@@ -131,7 +109,7 @@ class Scheduler(object):
 		else:
 			return False
 	
-	def select_job(self, doc , by="name"):
+	def show_by(self, doc , by="name"):
 		project_list = self.get_list({str(by): doc[str(by)]})
 		if project_list is not False:
 			print "***\tProject %s: %s\t***" %(str(by), str(doc[by]))
@@ -144,8 +122,7 @@ class Scheduler(object):
 			return project_list
 		else:
 			return False
-				
-			
+		
 	def run_job(self, job_name=None):
 		'''Execute tasks from Job Database'''
 		if job_name is not None:
@@ -163,64 +140,67 @@ class Scheduler(object):
 		
 class Job(object):
 	__metaclass__ = ABCMeta
-		
-	@staticmethod	
-	def create_from_ui(user_input):
-		'''Configure option of jobs'''
-		job = {}
-		job["action"] = None
-		job["update"] = None
+	
+	@privatemethod
+	def validate_ui(self, user_input):
 		#configure listing option if mail of owner or project_name
 		if user_input['<name>'] is not None:
-			
 			if validate_email(user_input['<name>']) is True:
 				job['user'] = user_input['<name>']
-				job['email'] = user_input['<name>']
+				job['name'] = None
+				job['action'] = "show"
 			else:
-				job['name'] = user_input['<name>']				
-				job["user"] = None
-				job['email'] = None
-			
-			for k,v in user_input.items():
-				k = re.sub("<|>|-|--", "", k)
-				
-				if k in ["report", "extract", "export", "delete", "archive", "crawl", "name"]:
-					if v is True:
-						job['action'] = k
-						job["start_date"] = datetime.today()
-				
-				elif k in ["q", "s", "k"]:
-					if v is True:
-						print "Updating existing job %s"%(job["name"])
-								
-				elif k in ["set", "append", "expand", "delete", "query", "email", "file", "url"]:
-					job[k] = v
-					if v is not None:
-						job["active"] = True
-						job["date"] = [datetime.today()]
-						job["configure"] = k
-					if k == "email":
-						if v is not None or False:
-							job["user"] = v
-							job["email"] = v
-				elif k in ["user"]:
-					if v is not None or False:
-						job["user"] = v
-				elif k in ["monthly", "weekly", "daily"]:
-					if v is not None or False:
-						job["frequency"] = v
-					else:
-						job["frequency"] = "monthly"
-				else:		
-					job[k] = v
+				job['name'] = user_input['<name>']
+				if user_input["-u"] is True:
+					job['user'] = user_input['<user>']
+				else:			
+					job["user"] = None
+				job['action'] = "show"
 			return job
-		
 		elif user_input['<url>'] is not None:
 			print "Archiving %s" %user_input['<url>']
 			raise NotImplementedError
 		else:
-			return False
-		
+			sys.exit()
+				
+	@staticmethod	
+	def create_from_ui(user_input):
+		'''Configure option of jobs'''
+		job = self.
+		for k,v in user_input.items():
+			k = re.sub("<|>|-|--", "", k)
+			
+			if k in ["report", "extract", "export", "delete", "archive", "crawl"]:
+				if v is True:
+					job['action'] = k
+					job["start_date"] = datetime.today()
+					job['update'] = False
+					
+			
+			elif k in ["q", "s", "k", "u"]:
+				if v is True:
+					#print "updating parameter '%s' in project '%s'"%(k, user_input["<name>"])
+					job['update'] = True
+					job['action'] = "update"
+					job['scope'] = k
+					
+			elif k in ["set", "append", "expand", "delete", "query", "user", "file", "url"]:
+				#job['scope'] = v
+				job['action'] = False
+				if v is not None:
+					job["active"] = True
+					job["date"] = [datetime.today()]
+					job["update"] = True
+					job[k] = v
+				
+			elif k in ["monthly", "weekly", "daily"]:
+				if v is not None or False:
+					job["frequency"] = v
+				else:
+					job["frequency"] = "monthly"
+			else:
+				job[k] = v
+		return job
 
 	@staticmethod	
 	def create_from_database(doc):
@@ -239,6 +219,40 @@ class Job(object):
 	def run(self):
 		print "running Job..."
 		pass
+				
+class CreateJob(Job):
+	def __init__(self, doc): 
+		#self.date = datetime.now()
+		
+		for k, v in doc.items():
+			if v is not None or False:
+				setattr(self,k,v)
+		self.action = "crawl"
+		self.status = "inactive"
+		self.active = False
+		#self.msg :"To activate crawl, you need to configure the project with minimum 2 required options:\n\t- A query\n\t- A list of url to crawl OR a search API Key\n" 
+		
+		
+		
+	def run(self):
+		new = yes_no("Do you want to create a new CRAWL project?")
+		if new == 1:		
+			task_db = Database(TASK_MANAGER_NAME)
+			coll = task_db.create_coll(TASK_COLL)
+			coll.insert(self.__dict__)
+			print "Project %s has been successfully created and scheduled!\n\t1/To see default parameters of the project:\n\tpython crawtext.py %s\n\t2/To add more parameters see help and options \n\tpython crawtext.py --help" %(self.name,self.name)
+			return True
+			
+class UpdateJob(Job):
+	def __init__(self, doc): 
+		self.date = datetime.now()
+		for k, v in doc.items():
+			setattr(self,k,v) 	
+	
+	def run(self):
+		print "updating"
+		print self.scope
+		pass	
 		
 class CrawlJob(Job):
 	def __init__(self, doc): 
@@ -370,6 +384,7 @@ class ExtractJob(Job):
 		for k, v in doc.items():
 			setattr(self,k,v) 	
 		pass
+		
 class ExportJob(Job):
 	def __init__(self, doc):
 		self.date = datetime.now()
