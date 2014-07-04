777c689445b3653e16fd1b0a1d68b21dcbb61ad2
C2_4b <4barbes@gmail.com>
2014-07-04 15:39:19 +0200

    Run crawl and stats

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
-from scheduler import Scheduler
+from scheduler4 import Scheduler
 
 if __name__ == "__main__":
 	s = Scheduler()
 	for n in s.get_list():
-		print n
-		if n["action"] == "crawl":
-			s.run_job(n)
+		
+		try:
+			#print n['name'], n['action']
+			s.run_job()
+		except Exception:
+			continue
+		#~ if n["action"] == "crawl":
+			#~ s.run_job(n)
 	
 	
 		
