d2aa31fb39401b0511432e7cc1acad8360cf2243
C2_4b <4barbes@gmail.com>
2014-08-13 23:41:02 +0200

    Testing scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,73 +1,55 @@
-import daemon # install python-daemon from pypi
-
-import sys
-import sched
-import time
-from datetime import datetime as dt
-import datetime
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from worker import Worker
-from job import *
-from database import Database
+from database import Database, TASK_COLL, TASK_MANAGER_NAME
+from datetime import datetime
+from threading import Timer
+from multiprocessing import Pool
+from job import Job
 
-def run_crawtext():
-	w = Worker()
-	for task in w.collection.find():
+def run_or_die(task):
+	today = datetime.today()
+	print today.second
+	if task["repeat"] == "hour":
+		if task["next_run"].hour == today.hour: return True
 		
-		job = w.create_from_database(task)
-		print job["repeat"]
-		
-	#~ db = Database(TASK_MANAGER_NAME)
-	#~ collection = db.use_coll(TASK_COLL)
-	#~ project_list = db.collection.find({"action":{"$or":["crawl", "archives"]})
-	#~ for n in project_list:
-		#~ print n
-	return 
-	#~ for n in project_list:
-		#~ j = Job(n)
-		#~ j2 = j.create_from_database()
-		#~ #try:
-		#~ print j2.		
-		#~ return j2.run()
-	#~ print "Running"
-	#~ w = Worker()
-	#~ return w.run_job()
-	
-	
-def now_str():
-    """Return hh:mm:ss string representation of the current time."""
-    t = dt.now().time()
-    return t.strftime("%H:%M:%S")
+	elif task["repeat"] == "day" or task["repeat"] == "week":
+		if task["next_run"].day == today.day: return True
+		return True
+	elif task["repeat"] == "month":
+		if task["next_run"].month == today.month and task["next_run"].day == today.day: 
+			return True
+		return False
+	elif task["repeat"] == "year":
+		if task["next_run"].year == today.year and task["next_run"].month == today.month and task["next_run"].day == today.day: return True
+		return False
+	else:
+		return False		
 
-
+def run_task(task):
+	if run_or_die(task) is True:
+		t = Job()
+		status = t.run(task)
+		db.job.update(task["_id"], {"$set":{"last_run_at":datetime.today(), "exec_time": t.elapsed, "status": status, "msg": t.logs}, "$inc": {"nb_run": 1}})
+		return True	
+	else:
+		return False
+def scheduler():
+	db = Database(TASK_MANAGER_NAME)
+	db.jobs = db.use_coll(TASK_COLL)
+	tasks = db.jobs.find()
+	pool = Pool(6)
+	pool.map(run_task, tasks)	
+	pool.close()
+	return pool.join()
+    
 def main():
-	def do_something_again(message):
-		print 'RUNNING:', now_str(), message
-		# Do whatever you need to do here
-		run_crawtext()
-		# then re-register task for same time tomorrow
-		t = dt.combine(dt.now() + datetime.timedelta(days=1), daily_time)
-		scheduler.enterabs(time.mktime(t.timetuple()), 1, do_something_again, ('Running again',))
-
-	# Build a scheduler object that will look at absolute times
-	scheduler = sched.scheduler(time.time, time.sleep)
-	print 'START:', now_str()
-	# Put task for today at 7am on queue. Executes immediately if past 7am
-	now = dt.now()
-	next_day = now
-	day =  now.day
-	next_day = now.replace(day=day+7)
-
-	daily_time = datetime.time(next_day)
-	first_time = dt.combine(now, daily_time)
-	# time, priority, callable, *args
-	scheduler.enterabs(time.mktime(first_time.timetuple()), 1,
-					   do_something_again, ('Run the first time',))
-	scheduler.run()
-
-if __name__ == '__main__':
-    main()
-    #if "-f" in sys.argv:
-    #    main()
-    #else:
-    #    with daemon.DaemonContext():
-    #        main()
+	x=datetime.today()
+	y=x.replace(day=x.day, hour=23, minute=39, second=10, microsecond=0)
+	delta_t=y-x
+	secs=delta_t.seconds+1
+	t = Timer(secs, scheduler)
+	t.start()
+	
+if __name__ == "__main__":
+	main()
